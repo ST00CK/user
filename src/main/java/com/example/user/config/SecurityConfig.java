bef124f3d54bf094ec2063c711201b7b7f5c9c74
HanKyungPark <156498970+HanKyungPark@users.noreply.github.com>
2025-01-22 16:51:26 +0900

    userdto 최신화

--- src/main/java/com/example/user/config/SecurityConfig.java
+++ src/main/java/com/example/user/config/SecurityConfig.java
@@ -43,17 +43,21 @@ public class SecurityConfig {
     private final KaKaoService kaKaoService;
     private final FormJwtUtill formjwtutil;
     private final AuthenticationConfiguration authenticationConfiguration;
-//    private final AuthenticationManager authenticationManager;
 
 
 
 
+
+    //인증 관리자
+    //필터로부터 인증처리를 지시받으면 가지고 있는 인증 처리자들 중에서 현재 인증처리를 할 수있는 Provider에게 인증처리를 위임하여 인증처리 수행후 인증 성공을 한다면 반환
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
 
+
+    //실질적으로 인증 절차가 이뤄지는 곳
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -62,21 +66,29 @@ public class SecurityConfig {
         return authProvider;
     }
 
+    //UserDetailsService로부터 DB에 저장된 사용자 정보와 비교 후 넘어온 절보를 가지고 Authentication 객체를 생성하거나 인증되지 못한경우에는 예외 처리를 해주는 역할을 한다.
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService(userMapper, formUserMapper);
     }
 
+    //Spring Security5.x버전부터 기존 xml 또는 체이닝 메서드 방식보다 람다 표현식을 활용한 구성방식이 도입
+    //가독성,간결성,유지보수성을 크게 향상 시키기 위해 도입됨
+    //기존 체이닝 방식으로 사용하면 코드가 길어지지만 람다식을 활용하면 메서드 체인 내부의 구조가 단순화되고 읽기 쉬워진다.
+    // (parameter) -> {function body}
+    // parameter = auth
+    // fucntion body = 입력값을 사용해 수행할 작업
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
-                    CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                    corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-                    corsConfiguration.setAllowCredentials(true);
-                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-                    corsConfiguration.setMaxAge(3600L);
+                    // CORS 설정을 커스터마이징하는 코드 시작.
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();// CORS 설정 객체 생성. 이 객체에 CORS 정책(허용 도메인, 메서드 등)을 설정
+                    corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));  // 허용할 도메인(Origin)을 설정
+                    corsConfiguration.setAllowedMethods(Collections.singletonList("*"));// 허용할 HTTP 메서드를 설정
+                    corsConfiguration.setAllowCredentials(true);// 자격 증명(쿠키, 인증 헤더 등)을 요청에 포함하는 것을 허용
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));// 클라이언트가 요청 시 보낼 수 있는 HTTP 헤더를 설정
+                    corsConfiguration.setMaxAge(3600L);// CORS 설정의 캐시 지속 시간
                     corsConfiguration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
                     return corsConfiguration;
                 }))
@@ -87,10 +99,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/kakao-token", "/login").authenticated()
                         .anyRequest().permitAll())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//세션을 생성하거나 사용하지않음 jwt토큰 방식이기 때문에 세션 x
 
 
 
+        // .class는 Spring Security에서 제공하는 필터로 커스텀한 필터를 앞에 붙이면 해당 필터보다 먼저 실행된다.
         // CustomRequestWrappingFilter 추가
         http.addFilterBefore(
                 new CustomRequestWrappingFilter(), UsernamePasswordAuthenticationFilter.class);
