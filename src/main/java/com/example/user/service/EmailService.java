a091a736f81867a6450c77237abb0f29bb41afab
HanKyungPark <156498970+HanKyungPark@users.noreply.github.com>
2025-02-11 14:40:12 +0900

    비밀번호 찾기 로직 리펙토링, 로그인 연동

--- src/main/java/com/example/user/service/EmailService.java
+++ src/main/java/com/example/user/service/EmailService.java
@@ -43,8 +43,8 @@ public class EmailService {
         return authCode.toString();
     }
 
-    // 이메일 전송 메서드
-    public void sendEmail(String to, String authCode) throws MessagingException {
+    //회원가입 메일
+    public void  sendEmailAndSaveAuthCode(String to, String authCode, HttpSession session) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 
@@ -52,46 +52,33 @@ public class EmailService {
         helper.setSubject("회원가입 인증 이메일");
         helper.setText(
                 "<h1>이메일 인증</h1>" +
-                        "<p>아래 코드를 회원가입 창에 입력해주세요.</p>" +
-                        "<h3>" + authCode + "</h3>",
-                true
-        );
-        helper.setFrom(fromEmail);
-
-        mailSender.send(message);
-    }
-    // 인증 코드 저장 (세션에 저장)
-    public void saveAuthCodeToSession(String email, String authCode, HttpSession session) {
-        session.setAttribute(email, authCode);
-    }
-
-    // 인증 코드 검증
-    public boolean verifyAuthCode(String email, String inputCode, HttpSession session) {
-        String savedCode = (String) session.getAttribute(email);
-
-        if (savedCode == null) {
-            throw new RuntimeException("인증 코드가 존재하지 않습니다. 이메일 인증을 다시 시도해주세요.");
-        }
-
-        return savedCode.equals(inputCode);
-    }
-
-
-    //비밀번호 변경 메서드
-    public void sendPasswordFindEmail(String to, String authCode) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
-
-        helper.setTo(to);
-        helper.setSubject("비밀번호 변경 이메일");
-        helper.setText(
-                "<h1>비밀번호변경 인증</h1>" +
                         "<p>본인인증 코드: " + authCode + "</p>",
                 true
         );
         helper.setFrom(fromEmail);
 
         mailSender.send(message);
+
+        // 인증 코드를 세션에 저장
+        session.setAttribute("authCode", authCode);
     }
 
+    //비밀번호 변경 메서드
+    public void sendPasswordFindEmail(String to, String authCode, HttpSession session) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+
+        helper.setTo(to);
+        helper.setSubject("비밀번호 변경 이메일");
+        helper.setText(
+                "<h1>비밀번호 변경 인증</h1>" +
+                        "<p>본인인증 코드: " + authCode + "</p>",
+                true
+        );
+        helper.setFrom(fromEmail);
+
+        mailSender.send(message);
+        // 인증 코드를 세션에 저장
+        session.setAttribute("authCode", authCode);
+    }
 }
